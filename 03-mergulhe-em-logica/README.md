2c990b7d974a923c4ea7ddf26e8fbbae0eb0dc60
Graielle Café <graziellecafe@aluno.ufop.edu.br>
2024-07-02 16:29:51 -0300

    feat:ternary method, random numbers

---
+++ 03-mergulhe-em-logica/README.md
@@ -0,0 +1,11 @@
+## Loops e tentativas 
+- Aprendemos a verificar se um número é maior ou menor do que outro utilizando estruturas condicionais (if/else) em nosso programa;
+
+- Utilizamos o loop "while" para repetir um bloco de código enquanto uma determinada condição for verdadeira, e permitir assim que o programa execute uma ação várias vezes;
+
+- Implementamos um contador de tentativas para acompanhar e exibir a quantidade de vezes que o usuário tentou adivinhar um número secreto. Podemos fazer isso, por exemplo, em um jogo de adivinhação.
+
+## Boas práticas de programação 
+- Operador ternário 
+- Número aleatório 
+- Break 
--- 03-mergulhe-em-logica/app.js
+++ 03-mergulhe-em-logica/app.js
@@ -1,12 +1,26 @@
 alert('Boas vindas ao jogo do número secreto'); 
-let numeroSecreto = 4; 
+let numeroMaximo = 5000; 
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);  
 console.log(numeroSecreto);
 
-let numeroChute = prompt('Escolha um número entre 1 e 10'); 
+let numeroChute; 
+let tentativas = 1; 
 
-if (numeroChute == numeroSecreto){
-    alert(`Parabéns, você acertou! O número secreto é ${numeroSecreto}`);
-} else {
- 
-    alert('Infelizmente você errou. Tente novamente')
+while (numeroChute != numeroSecreto) {
+    numeroChute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
+    console.log(numeroChute); 
+
+    if (numeroChute == numeroSecreto){
+        break; 
+    } else { 
+        if(numeroChute > numeroSecreto){
+            alert(`O número secreto é menor que ${numeroChute}`); 
+        } else {
+            alert(`O número secreto é maior que ${numeroChute}`);
+        }
+    } tentativas++; 
 }
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'; 
+alert(`Parabéns, você acertou! O número secreto é ${numeroSecreto} e você acertou utilizando ${tentativas} ${palavraTentativa} `);
+
